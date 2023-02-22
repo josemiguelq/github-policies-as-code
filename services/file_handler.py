93e2f21d3d2684b2e7dfb1965b690c9c036905e9
Jose Miguel <jose.miguelq95@gmail.com>
2023-02-22 18:55:59 -0300

    added teams

--- services/file_handler.py
+++ services/file_handler.py
@@ -15,12 +15,12 @@ def read_repos():
     return repositories
 
 def read_teams():
-    repositories_path = './resources/teams'
-    onlyfiles = [f for f in listdir(repositories_path) if isfile(join(repositories_path, f))]
-    repositories = []
+    teams_path = './resources/teams'
+    onlyfiles = [f for f in listdir(teams_path) if isfile(join(teams_path, f))]
+    teams = []
     for file in onlyfiles:
         content = open(join(repositories_path, file))
         repo = file.replace('.json', '')
         settings = json.loads(content.read())
-        repositories.append({"name" : repo, "settings": settings})
+        teams.append({"name" : repo, "settings": settings})
     return repositories
